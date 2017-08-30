a399c9efbb0cc4ccd24c46b5a1ef298cfeac95b9
Cristian Sima <cristian.sima93@yahoo.com>
2017-08-30 21:54:04 +0300

    added versions

--- client/reducers/versions.js
+++ client/reducers/versions.js
@@ -1,26 +1,34 @@
 // @flow
 
-import type { Action, ModalState } from "types";
+import type { Action, ModalState, State } from "types";
 
 import * as Immutable from "immutable";
+import { createSelector } from "reselect";
 
 const initialState = Immutable.Map();
 
-// const
-//   showModal = (state : ModalState, { payload : { modalType, modalProps } }) => (
-//     state.push({
-//       type  : modalType,
-//       props : modalProps,
-//     })
-//   );
+const
+  fetchItemFulFilled = (state : ModalState, { payload : { item, versions } }) => (
+    state.set(item.get("_id"), versions.entities)
+  );
 
 const reducer = (state : ModalState = initialState, action : Action) => {
   switch (action.type) {
 
+    case "FETCH_ITEM_DETAILS_FULFILLED":
+      return fetchItemFulFilled(state, action);
 
     default:
       return state;
   }
 };
+
+const getData = (state : State) => state.versions;
+
+export const getVersionsOfItem = createSelector(
+  getData,
+  (state, id) => id,
+  (data, id) => data.get(id)
+);
 
 export default reducer;
